7ebcc126f12ae65fa8c2c33ca434ee9174067723
dainnida <dain0928@g.hongik.ac.kr>
2024-04-10 21:13:54 +0900

    first commit

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "dilivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // string으로 필수
+    private DeliveryStatus status; // ENUM[READY(준비), COMP(배송)]
+}
